3adf4f18d577a341478e2b0b693f2404e8c96da6
Nikepoi <rarwzeno@gmail.com>
2025-06-20 08:43:14 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'files.catbox.moe',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+};
